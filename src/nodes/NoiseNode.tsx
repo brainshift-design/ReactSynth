79ae18b7e582b0f92f2bdca646c9d8d5710f60c2
shamsi <brainshift.design@gmail.com>
2025-01-03 19:47:55 +0100

    added Noise node (#46)

---
+++ src/nodes/NoiseNode.tsx
@@ -0,0 +1,79 @@
+import nodeStyles from './Node.module.css';
+import { NodeProps, Position } from "reactflow";
+import { audioContext } from "../audio/audio";
+import AudioNode from "./AudioNode";
+import OutputHandle from '../components/OutputHandle';
+
+
+
+export default class NoiseNode extends AudioNode<NodeProps>
+{
+    protected createAudioNode()
+    {
+        return audioContext?.createBufferSource() as globalThis.AudioBufferSourceNode;
+    }
+
+
+
+    protected override initAudioNode()
+    {
+        const node = this.audioNode as globalThis.AudioBufferSourceNode;
+
+        if (node)
+        {
+            node.buffer = createNoiseBuffer(1);
+            node.loop   = true;
+            
+            node.start();
+        }
+    }
+
+
+
+    renderContent()
+    {
+        return (
+            <>
+                <h1>Noise</h1>
+
+                <div className={nodeStyles.nodeContent}>
+
+                    <div 
+                        className ={nodeStyles.checkers}
+                        style     = 
+                        {{
+                            width:  40,
+                            height: 40,
+                            margin: '0 10px 10px 10px'
+                        }}>
+
+                    </div>
+
+                </div>
+
+                <OutputHandle 
+                    type     = 'source' 
+                    position = {Position.Right} 
+                    id       = {'audio-in'} 
+                    nodeid   = {this.props.id} 
+                />
+            </>
+        );
+    }
+}
+
+
+
+function createNoiseBuffer(duration: number)
+{
+    const sampleRate  = audioContext?.sampleRate;
+    const bufferSize  = sampleRate! * duration;
+
+    const buffer      = audioContext?.createBuffer(1, bufferSize, sampleRate!);
+    const channelData = buffer?.getChannelData(0);
+
+    for (let i = 0; i < bufferSize; i++)
+        channelData![i] = Math.random() * 2 - 1;
+
+    return buffer!;
+}
